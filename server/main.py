0f63e27dda07a091328a6d4884c67a05cb512fe3
Fatima Sanchez Suarez <fatima.sanchez@loadsmart.com>
2024-12-14 21:35:02 -0600

    add server files

---
+++ server/main.py
@@ -0,0 +1,42 @@
+import socket
+from message import send_whatsapp_notification, MESSAGE_WAIT_MINS
+from datetime import datetime, timedelta
+import json
+
+HOST = "192.168.1.89"
+PORT = 65432
+
+last_message_sent_at = datetime.now()
+list_events = []
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("server listening...")
+
+    try:
+        while 1:
+            client, clientInfo = s.accept()
+            print("server recv from: ", clientInfo)
+            data = client.recv(1024)  # receive 1024 Bytes of message in binary format
+            print("received data: ", data)
+            event = data.decode("ascii")
+            list_events.append(json.loads(event))
+            print(list_events)
+
+            time_now = datetime.now()
+            should_send_message_at = last_message_sent_at + timedelta(
+                minutes=MESSAGE_WAIT_MINS
+            )
+
+            print(time_now)
+            print(should_send_message_at)
+
+            if len(list_events) > 0: #and (time_now >= should_send_message_at):
+                send_whatsapp_notification(list_events)
+                list_events = []
+                last_message_sent_at = datetime.now()
+
+    except SystemError:
+        print("Closing socket")
+        client.close()
+        s.close()
---
+++ server/message.py
@@ -0,0 +1,41 @@
+import os
+from twilio.rest import Client
+from dotenv import main
+from datetime import datetime
+
+main.load_dotenv()
+
+ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+FROM_WHATSAPP_NUMBER = os.getenv("TWILIO_FROM_NUM")
+TO_WHATSAPP_NUMBER = os.getenv("TWILIO_TO_NUM")
+
+# Minutes to send message
+MESSAGE_WAIT_MINS = 1
+HOURS_FORMAT = "%H:%M:%S %p"
+date_str = '2023-02-28 14:30:00'
+date_format = '%Y-%m-%d %H:%M:%S.%f'
+
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+def client_send_message(message):
+    print("Sending message...")
+    print(message)
+    client.messages.create(
+        body=message, from_=FROM_WHATSAPP_NUMBER, to=TO_WHATSAPP_NUMBER
+    )
+    print("Message successfully sent...")
+
+
+def send_whatsapp_notification(list_events):
+    print("Sending notification...")
+    # Build message from events list
+    message = "Your cat has been seen in the following spots: \n"
+    for event in list_events:
+        timestamp = datetime.strptime(event.get("timestamp"), date_format)
+        location = event.get("location")
+        message += f"* {timestamp.strftime(HOURS_FORMAT)} - {location} \n"
+    client_send_message(message)
+
+
